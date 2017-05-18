78b324ca3439571f6ac1bc31d7e5dfc5bf56fa39
sofiekhullar <sofie_k@live.se>
2017-05-18 17:09:20 +0900

    Working after tutorial

--- src/components/views/home.jsx
+++ src/components/views/home.jsx
@@ -1,14 +1,58 @@
 import React, { Component } from "react";
 import { browserHistory } from 'react-router';
+import GifList from './GifList';
+import SearchBar from './SearchBar';
+import request from 'superagent';
+import GifModal from './GifModal';
 
 export default class Home extends Component {
   componentDidMount() {
     browserHistory.push('/');
   }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGif: null,
+      modalIsOpen: false
+
+    }
+    this.handleTermChange = this.handleTermChange.bind(this);
+  }
+  
+  openModal(gif) {
+    this.setState({
+      modalIsOpen: true,
+      selectedGif: gif
+    });
+  }
+
+  closeModal() {
+    this.setState({
+      modalIsOpen: false,
+      selectedGif: null
+    });
+  }
+
+   handleTermChange(term) {
+    const url = `http://api.giphy.com/v1/gifs/search?q=${term.replace(/\s/g, '+')}&api_key=dc6zaTOxFJmzC`;
+
+    request.get(url, (err, res) => {
+      this.setState({ gifs: res.body.data })
+    });
+  }
+
   render() {
     return (
       <div id="home">
-        This is YoloTrip
+        <h1>YoloTrip</h1>
+            <SearchBar onTermChange={term => this.handleTermChange(term)} />
+             <GifList  gifs={this.state.gifs}
+                  onGifSelect={selectedGif => this.openModal(selectedGif) } />
+            <GifModal modalIsOpen={this.state.modalIsOpen}
+                  selectedGif={this.state.selectedGif}
+                  onRequestClose={ () => this.closeModal() } />
       </div>
     );
   }
